eb7b2853ec3f16390e5707c136a0e88662bd31a7
Thanoja Thangaraja <thanujathanuja18@yahoo.com>
2023-02-25 22:32:20 +0000

    adding of child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Marguerite',
+      imageUrl: '../assets/marguerite.jpeg',
+      username: 'Elegant',
+      content: 'Prune marguerite daisies by deadheading spent flowers and cutting away dead foliage to promote new growth and increase the bushiness of the shrubs',
+    },
+
+    {
+      title: 'Pink Cherry',
+      imageUrl: '../assets/pink-cherry-blossoms.jpeg',
+      username: 'Beautiful',
+      content: 'Cherries have a lot of antioxidants and anti-inflammatory compounds. They might boost your heart health. They could help you sleep. Cherries can boost your exercise performance (and recovery).'
+    },
+
+    {
+      title: 'Rose',
+      imageUrl: '../assets/rose.jpeg',
+      username: 'Georgeous',
+      content: 'Rose petals contain polyphenols, antioxidants that work to protect your body from cell damage. The polyphenols in rose tea have been shown to reduce the risk of heart disease, diabetes, obesity, and cognitive diseases.'
+    }
+  ];
 }
